d9390a9f5518c18bd538179463e22ee0f7fbd383
brupadiro <brunodiharce@gmail.com>
2023-06-30 16:52:39 -0300

    cambio url

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://puncable.codiq.net'
+    baseURL: 'https://puntacable.abunda.uy'
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
